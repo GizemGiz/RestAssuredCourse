e5c96210d337da4d1443c945421b41c25e9056d4
gizem <gizemmcevirgen@gmail.com>
2022-09-20 15:33:46 -0500

    Go rest user test

--- src/test/java/GoRestUserTest.java
+++ src/test/java/GoRestUserTest.java
@@ -16,35 +16,65 @@ public class GoRestUserTest {
     private Object userId;
 
     @BeforeClass
-    public void setup(){
+    public void setup() {
 
         RestAssured.baseURI = "https://gorest.co.in";
 
-        reqSpec =given()
+        reqSpec = given()
                 .log().uri()
-                .header("Authorization", "Bearer ba84af0513873344544a5fa2a33b76e8d2d892d89b74322a3df05ef647fae66")
+                .header("Authorization", "Bearer ba84af0513873344544a5fa2a33b76e8d2d892d89b74322a3df05ef647fae66e")
                 .contentType(ContentType.JSON);
 
         requestBody = new HashMap<>();
-        requestBody.put("name", "Tester Test User");
-        requestBody.put("email", "testeruser@gmail.com");
+        requestBody.put("name", "BlahBlah123");
+        requestBody.put("email", "blahblah@technoblah.com");
         requestBody.put("gender", "female");
         requestBody.put("status", "active");
+
     }
 
     @Test
-    public void createUserTest(){
+    public void createUserTest() {
 
-         userId = given()
+        userId = given()
                 .spec(reqSpec)
                 .body(requestBody)
                 .when()
                 .post("/public/v2/users")
                 .then()
                 .log().body()
+                .body("name", equalTo(requestBody.get("name")))
                 .statusCode(201)
                 .extract().path("id");
     }
 
-    @Test
+    @Test(dependsOnMethods = "createUserTest")
+    public void editUserTest() {
+
+        HashMap<String, String> updateRequestBody = new HashMap<>();
+        updateRequestBody.put("name", "Updated Blah Blah name");
+
+        given()
+                .spec(reqSpec)
+                .body(updateRequestBody)
+                .when()
+                .put("/public/v2/users/" + userId)
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test(dependsOnMethods = "editUserTest")
+    public void deleteUserTest() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .delete("/public/v2/users/" + userId)
+                .then()
+                .log().body()
+                .statusCode(204);
+
+
+    }
 }
