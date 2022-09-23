1cb70351a74a756b24aa46f688ddb4ecc5dcbc6c
gizem <gizemmcevirgen@gmail.com>
2022-09-23 14:59:14 -0500

    Campus API Automation - Nationality  Functionality

--- src/test/java/NationalitiesFunctionTests.java
+++ src/test/java/NationalitiesFunctionTests.java
@@ -13,6 +13,7 @@ import static org.hamcrest.Matchers.*;
 
 public class NationalitiesFunctionTests {
     private RequestSpecification reqSpec;
+    private String nationality_id;
     private Cookies cookies;
 
 
@@ -26,31 +27,60 @@ public class NationalitiesFunctionTests {
                 .contentType(ContentType.JSON);
     }
 
-    @Test
+    @Test(priority = 1)
     public void loginTest() {
 
         HashMap<String, String> credentials = new HashMap<>();
+        credentials.put("username", "richfield.edu");
+        credentials.put("password", "Richfield2020!");
+        credentials.put("rememberMe", "true");
 
-        given()
+        cookies = given()
                 .spec(reqSpec)
                 .body(credentials)
                 .when()
                 .post("/auth/login")
                 .then()
+                .body("username", equalTo(credentials.get("username")))
                 .statusCode(200)
                 .extract().detailedCookies();
     }
 
-    @Test(dependsOnMethods = "loginTest")
+    @Test(priority = 2)
     public void createNationalityTest(){
 
         HashMap<String, String> reqBody = new HashMap<>();
-        //reqBody.get("nationality", "Turkey");
+        reqBody.put("name", "Gizem Nation");
+
+
+        nationality_id = given()
+                .spec(reqSpec)
+                .cookies(cookies)
+                .body(reqBody)
+                .when()
+                .post("/school-service/api/nationality")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .extract().jsonPath().getString("id");
+
+
+    }
+    @Test(priority = 3)
+    public void createNationalityNegativeTest(){
+
+        HashMap<String, String> reqBody = new HashMap<>();
+        reqBody.put("name", "Gizem Nation");
+
         given()
                 .spec(reqSpec)
                 .cookies(cookies)
+                .body(reqBody)
                 .when()
+                .post("/school-service/api/nationality")
                 .then()
-                .statusCode(200);
+                .log().body()
+                .statusCode(400);
     }
+
 }
